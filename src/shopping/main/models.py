65dd56d4d83a1217ae7d9f250bea77ad569dc6f7
noselak <dworanowski.adrian@gmail.com>
2017-06-05 12:16:23 +0000

    02 - Added main model

---
+++ src/shopping/main/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=40)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+
+class ShoppingList(models.Model):
+    name = models.CharField(max_length=40)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    shop = models.CharField(max_length=40)
+    date = models.DateField()
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    
+    def __str__(self):
+        return self.name
+    
+
+class ShoppingItem(models.Model):
+    item = models.ForeignKey('Item', on_delete=models.CASCADE)
+    shopping_list = models.ForeignKey('ShoppingList', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.item.name
